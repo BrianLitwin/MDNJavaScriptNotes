2f17095ba39a80a37549876bf083bb079c0f0a11
Brian Litwin <brian.n.litwin@gmail.com>
2019-01-24 18:20:53 -0500

    Create ArrayMethods.js

---
+++ ArrayMethods.js
@@ -0,0 +1,34 @@
+#### Array Methods 
+
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Indexed_collections
+* concat() 
+* join(delimiter=', ')
+* push() 
+* pop() 
+* shift() 
+* unshift() 
+* slice 
+* splice 
+* reverse() 
+* sort()
+* indexOf()
+* lastIndexOf()
+* forEach()
+* map()
+* filter()
+* every()
+* some()
+* reduce()
+* reduceRight
+
+#### How to 
+https://sdras.github.io/array-explorer/
+
+* combine two arrays and return a new array 
+* add elements to an array 
+* add to the beginning of an array 
+* add to the end of an array 
+* remove elements from an array
+* remove from beginning 
+* remove from end 
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
